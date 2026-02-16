90d4942e2b50ab1ccce94d0222b2f2925668fd78
Atikgit <codera1a1@gmail.com>
2026-02-16 08:40:34 +0600

    Add files via upload

--- download_tts.py
+++ download_tts.py
@@ -8,11 +8,18 @@ import shutil
 TTS_MODELS = {
     "ben": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-ben.tar.bz2",
     "ara": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-ara.tar.bz2",
-    "eng": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-piper-en_US-amy-low.tar.bz2",
     "hin": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-hin.tar.bz2",
     "spa": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-spa.tar.bz2",
     "fra": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-fra.tar.bz2",
     "deu": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-deu.tar.bz2",
+    "ita": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-ita.tar.bz2",
+    "rus": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-rus.tar.bz2",
+    "kor": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-kor.tar.bz2",
+    "jpn": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-jpn.tar.bz2",
+    "por": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-por.tar.bz2",
+    "tur": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-tur.tar.bz2",
+    "vie": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-vie.tar.bz2",
+    "urd": "https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-mms-urd.tar.bz2",
     # ... এখানে আপনি আরও লিংক যোগ করতে পারেন
 }
 
